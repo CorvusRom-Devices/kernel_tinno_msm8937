4ca94be9a3d7d721c711c28182f8f1f1711acde1
Louis Popi <theh2o64@gmail.com>
2018-07-12 12:38:45 +0200

    base: Load firmware directly from /system/etc/firmware/

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -299,6 +299,9 @@ static void fw_free_buf(struct firmware_buf *buf)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+	#ifdef CONFIG_PLATFORM_V12BN
+	"/system/etc/firmware/",
+	#endif
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
