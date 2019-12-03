TINNO-8937/android_kernel_tinno_msm8937

arm64: wimlite: Use the frozen N camera driver

include/media/msm_cam_sensor.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_cam_sensor_n.h"
 #else
 #ifndef __LINUX_MSM_CAM_SENSOR_H
@@ -283,4 +283,4 @@ struct msm_flash_cfg_data_t32 {
 #endif
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/media/msmb_isp.h
@@ -9,7 +9,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_isp_n.h"
 #else
 #ifndef __MSMB_ISP__
@@ -46,4 +46,4 @@ struct msm_isp32_event_data32 {
 
 #endif
 #endif /* __MSMB_ISP__ */
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msm_cam_sensor.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_cam_sensor_n.h"
 #else
 #ifndef __UAPI_LINUX_MSM_CAM_SENSOR_H
@@ -610,4 +610,4 @@ struct sensor_init_cfg_data {
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t)
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msm_camsensor_sdk.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_camsensor_sdk_n.h"
 #else
 #ifndef __UAPI_LINUX_MSM_CAMSENSOR_SDK_H
@@ -423,4 +423,4 @@ struct msm_camera_i2c_reg_setting_array {
 };
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msmb_camera.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_camera_n.h"
 #else
 #ifndef __UAPI_LINUX_MSMB_CAMERA_H
@@ -227,4 +227,4 @@ struct msm_camera_private_ioctl_arg {
 	_IOWR('V', BASE_VIDIOC_PRIVATE, struct msm_camera_private_ioctl_arg)
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msmb_isp.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_isp_n.h"
 #else
 #ifndef __UAPI_MSMB_ISP__
@@ -1095,4 +1095,4 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)
 
 #endif/* __MSMB_ISP__ */
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

CorvusRom-Devices/kernel_tinno_msm8937

include/media/msm_cam_sensor.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_cam_sensor_n.h"
 #else
 #ifndef __LINUX_MSM_CAM_SENSOR_H
@@ -282,4 +282,4 @@ struct msm_flash_cfg_data_t32 {
 #endif
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/media/msmb_isp.h
@@ -9,7 +9,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_isp_n.h"
 #else
 #ifndef __MSMB_ISP__
@@ -46,4 +46,4 @@ struct msm_isp32_event_data32 {
 
 #endif
 #endif /* __MSMB_ISP__ */
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msm_cam_sensor.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_cam_sensor_n.h"
 #else
 #ifndef __UAPI_LINUX_MSM_CAM_SENSOR_H
@@ -608,4 +608,4 @@ struct sensor_init_cfg_data {
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t)
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msm_camsensor_sdk.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msm_camsensor_sdk_n.h"
 #else
 #ifndef __UAPI_LINUX_MSM_CAMSENSOR_SDK_H
@@ -421,4 +421,4 @@ struct msm_camera_i2c_reg_setting_array {
 };
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msmb_camera.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_camera_n.h"
 #else
 #ifndef __UAPI_LINUX_MSMB_CAMERA_H
@@ -227,4 +227,4 @@ struct msm_camera_private_ioctl_arg {
 	_IOWR('V', BASE_VIDIOC_PRIVATE, struct msm_camera_private_ioctl_arg)
 
 #endif
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/

include/uapi/media/msmb_isp.h
@@ -1,4 +1,4 @@
-#ifdef CONFIG_PROJECT_GARLIC
+#if defined(CONFIG_PROJECT_GARLIC) || defined(CONFIG_PROJECT_WIMLITE)
 #include "msmb_isp_n.h"
 #else
 #ifndef __UAPI_MSMB_ISP__
@@ -1099,4 +1099,4 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)
 
 #endif/* __MSMB_ISP__ */
-#endif /* CONFIG_PROJECT_GARLIC */
+#endif /* CONFIG_PROJECT_GARLIC || CONFIG_PROJECT_WIMLITE*/